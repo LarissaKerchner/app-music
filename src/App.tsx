1b7c127a035cffe98262cc596569ee8b8cf82ef4
Larissa Kerchner <kerchnerchagaslarissa@gmail.com>
2023-08-31 20:03:17 -0300

    fix: remoção de arquivos css vazias

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Search from './pages/search/Search';
+import Album from './pages/album/Album';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
       <Route path="/search" element={ <Search /> } />
-      {/* <Route path="/album/:id" element={ <Album /> } /> */}
+      <Route path="/album/:id" element={ <Album /> } />
       {/* <Route path="/favorites" element={ <Favorites /> } />
       <Route path="/profile" element={ <Profile /> } />
       <Route path="/profile/edit" element={ <ProfileEdit /> } />
